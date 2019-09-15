af8b36c0246f81deb709f180abb28d7038dcab82
cesar abel <abel@toruk.toruk.lo>
2019-09-14 20:32:14 -0600

    mounting react

--- django_react_assessment/django_react_assessment/urls.py
+++ django_react_assessment/django_react_assessment/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('movies.urls')),
+    path('api', include('movies.urls')),
+    path('', include('frontend.urls')),
 ]
